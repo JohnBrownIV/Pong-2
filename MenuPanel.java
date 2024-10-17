7923aa5a6d8d2ae90de40f162f9f44c4625f68f7
John Brown <100054328@csisd.org>
2024-10-17 11:32:56 -0500

    button 1 && 2 display

--- MenuPanel.java
+++ MenuPanel.java
@@ -18,12 +18,22 @@ public class MenuPanel extends JPanel {
 
   g2D.fillRect(0, 0, 1300, 800);
   //Name
-  g2D.setFont(new Font("Comic Sans", 1,100));
+  g2D.setFont(new Font("Comic Sans MS", 1,100));
   g2D.setPaint(Color.white);
   g2D.drawString("PONG 2", 460, 150);
   //Button 1
   g2D.fillRect(450, 200, 400, 100);
   g2D.setColor(Color.black);
   g2D.fillRect(460, 210, 380, 80);
+  g2D.setColor(Color.white);
+  g2D.setFont(new Font("Comic Sans MS", 1,50));
+  g2D.drawString("1 PLAYER", 525, 270);
+  //Button 2
+  g2D.fillRect(450, 350, 400, 100);
+  g2D.setColor(Color.black);
+  g2D.fillRect(460, 360, 380, 80);
+  g2D.setColor(Color.white);
+  g2D.setFont(new Font("Comic Sans MS", 1,50));
+  g2D.drawString("2 PLAYERS", 515, 420);
  }
 }
